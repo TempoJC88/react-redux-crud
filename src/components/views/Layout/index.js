52ba4a7c598a1585de3f2ba8e192a12a406afd55
JoseCamargo <joseluis.camargo@i3television.es>
2018-08-17 08:57:30 +0200

    datos del contacto que se va a visualizar en el detalle  y búsqueda del contacto a mostrar

--- src/components/views/Layout/index.js
+++ src/components/views/Layout/index.js
@@ -25,12 +25,12 @@ class Layout extends Component {
     return (
       <div>
         <nav className="green lighten-1" role="navigation">
-          <div class="nav-wrapper">
-            <span class="brand-logo center">{title}</span>
-            <ul class="right hide-on-med-and-down">              
-              <li><NavLink  to="/listPets"><i class="Large material-icons">pets</i></NavLink></li>
-              <li><NavLink  to="/createPet"><i class="Large material-icons">plus_one</i></NavLink></li>
-              <li><NavLink  to="/"><i class="Large material-icons">home</i></NavLink></li>
+          <div className="nav-wrapper">
+            <span className="brand-logo center">{title}</span>
+            <ul className="right hide-on-med-and-down">              
+              <li><NavLink  to="/listPets"><i className="Large material-icons">pets</i></NavLink></li>
+              <li><NavLink  to="/createPet"><i className="Large material-icons">plus_one</i></NavLink></li>
+              <li><NavLink  to="/"><i className="Large material-icons">home</i></NavLink></li>
             </ul>
           </div>
         </nav>
--- src/components/views/Pets/Detail.js
+++ src/components/views/Pets/Detail.js
@@ -3,36 +3,67 @@ import React, {Component} from 'react'
 class Detail extends Component {
     constructor(props){
         super();
-        console.log( "Detail");        
-        //this.context.router.history.push('/detailPet/'+id);
-        //props.match.params.id
-
+        
         this.state={
-            pet:{
-                raza: "",
-                nombre: "",
-                id: "",
-                fechaNac:"" 
-            }
+            pet:props.pets.pets.result.find( pet => pet.id === parseInt(props.match.params.id))
         }
     }
 
+    getDateFormat(fecha){
+        let date = new Date(fecha);
+        let year = date.getFullYear();
+        let month = date.getMonth()+1;
+        let dt = date.getDate();
+
+        if (dt < 10) {
+            dt = '0' + dt;
+        }
+        if (month < 10) {
+            month = '0' + month;
+        }
+        return year+'-' + month + '-'+dt;
+    }
+
     render() {        
+        let dateFormat = this.getDateFormat(this.state.pet.fechaNac);
         return (
             <section>                
                 <div className="row">
-                    <form className="col s12">
+                    <div className="col s12">
                         <div className="row">
-                            <div className="input-field col s6">
-                            <i className="material-icons prefix">account_circle</i>                            
-                            <label>First Name</label>
+                            <div className="input-field col s6">                            
+                                <label>{this.state.pet.Nombre}</label>
                             </div>
-                            <div className="input-field col s6">
-                            <i className="material-icons prefix">phone</i>                            
-                            <label>Telephone</label>
+                            <div className="input-field col s6">                            
+                                <label>{dateFormat}</label>
                             </div>
                         </div>
-                    </form>
+                        <div className="row">
+                            <div className="input-field col s6">                            
+                                <label>{this.state.pet.raza}</label>
+                            </div>
+                            <div className="input-field col s6">                            
+                                <label>{this.state.pet.sexo}</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s3">                            
+                                <label>{this.state.pet.tamagno}</label>
+                            </div>
+                            <div className="input-field col s3">                            
+                                <label>{this.state.pet.especie}</label>
+                            </div>
+                            <div className="input-field col s3">                            
+                                <label>{this.state.pet.Color}</label>
+                            </div>     
+                        </div>                        
+                       
+                        <div className="row">
+                            <div className="input-field col s12">                                
+                                <label htmlFor="textarea2">{this.state.pet.observaciones}</label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>            
         );
