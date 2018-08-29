54e295753b44581f98aa7655391e3b91e7a58603
DESKTOP-42B15Q6\tempo <josel.cc.dam@gmail.com>
2018-08-29 21:14:35 +0200

    Pruebas para crear

--- src/components/views/Pets/Create.js
+++ src/components/views/Pets/Create.js
@@ -1,58 +1,102 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 
 class Create extends Component {
-    constructor(props){
+    constructor(props) {
         super();
 
-        console.log( props.pets.pets.result );
-        this.state={
-            pet:{
+        console.log(props.pets.pets.result);
+        this.state = {
+            pet: {
                 raza: "",
                 nombre: "",
                 id: "",
-                fechaNac:"" 
+                fechaNac: ""
             },
             fireRedirect: false,
             errors: {}
         }
     }
 
-    handleInput(event){
+    handleInput(event) {
         let value = event.target.value;
         let nameInput = event.target.name;
         let pet = this.state.pet;
         pet[nameInput] = value;
-        this.setState({pet});
+        this.setState({ pet });
     }
 
-    Crear(){
-        this.props.Save(this.state.pet); 
-        this.setState({fireRedirect:true});
+    Crear() {
+        this.props.Save(this.state.pet);
+        this.setState({ fireRedirect: true });
     }
 
-    render() {        
+    render() {
         return (
-            <section>                
+            <section>
                 <div className="row">
-                    <form className="col s12">
-                        <div className="row">
-                            <div className="input-field col s6">
-                                <input placeholder="Raza" name="raza" type="text" className="validate" value={this.state.pet.raza} onChange={this.handleInput.bind(this)} />                                
+                    <div className="col s12">
+                        <div className="col s4">
+                            <div>
+                                <img src={foto} className="responsive-image" alt={raza} />
                             </div>
-                                <div className="input-field col s6">                                
-                                <input placeholder="Nombre" name="nombre" type="text" className="validate" value={this.state.pet.nombre} onChange={this.handleInput.bind(this)} />                                
-                            </div>                            
-                        </div>        
-
-                        <button class="btn waves-effect waves-light" type="submit" onClick={this.Crear.bind(this)} name="action">Crear
-                            <i class="material-icons right">send</i>
+                        </div>
+                        <div className="input-field col s4">
+                            <div>
+                                <label htmlFor='nombre' className="orange-text text-lighten-2">Nombre:</label>
+                                <input value={nombre} id='nombre' type="text" className="validate" />
+                            </div>
+                        </div>
+                        <div className="input-field col s4">
+                            <div>
+                                <label htmlFor='fechaNacimiento' className="orange-text text-lighten-2">Fecha nacimiento:</label>
+                                <input value={datehtmlFormat} id='fechaNacimiento' type="text" className="validate" />
+                            </div>
+                        </div>
+                        <div className="input-field col s4">
+                            <div>
+                                <label htmlFor='raza' className="orange-text text-lighten-2">Raza:</label>
+                                <input value={raza} id='raza' type="text" className="validate" />
+                            </div>
+                        </div>
+                        <div className="input-field col s4">
+                            <div>
+                                <label htmlFor='especie' className="orange-text text-lighten-2">Especia:</label>
+                                <input value={especie} id='especie' type="text" className="validate" />
+                            </div>
+                        </div>
+                        <div className="input-field col s4">
+                            <div>
+                                <label htmlFor={id + ficha + sexo} className="orange-text text-lighten-2">Sexo:</label>
+                                <input value={sexo} id={id + ficha + sexo} type="text" className="validate" />
+                            </div>
+                        </div>
+                        <div className="input-field col s4">
+                            <div>
+                                <label htmlFor={id + ficha + tamagno} className="orange-text text-lighten-2">Tamaño:</label>
+                                <input value={tamagno} id={id + ficha + tamagno} type="text" className="validate" />
+                            </div>
+                        </div>
+                        <div className="input-field col s4">
+                            <div>
+                                <label htmlFor={id + ficha + color} className="orange-text text-lighten-2">Color:</label>
+                                <input value={color} id={id + ficha + color} type="text" className="validate" />
+                            </div>
+                        </div>
+                        <div className="input-field col s12">
+                            <div>
+                                <label className="orange-text text-lighten-2">Observaciones:</label>
+                                <textarea rows="5" max-rows="20" className="materialize-textarea" style={{ 'borderBottom': '0px' }} >{observaciones}</textarea>
+                            </div>
+                        </div>
+                        <button className="btn waves-effect waves-light" type="submit" onClick={this.Crear.bind(this)} name="action">Crear
+                            <i className="material-icons right">send</i>
                         </button>
-              
-                        {this.state.fireRedirect && ( <Redirect to="/listPets" /> )}
-                    </form>
+
+                        {this.state.fireRedirect && (<Redirect to="/listPets" />)}
+                    </div>
                 </div>
-            </section>            
+            </section>
         );
     }
 }
